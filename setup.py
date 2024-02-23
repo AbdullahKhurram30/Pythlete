a0d69c7d33397d1b699db711a1597a2ec9fe0339
Abdullah <abdullah.khurram@uni.minerva.edu>
2024-02-23 10:42:11 +0000

    initial commit for set up of the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythlete',
+    version='0.1.0',
+    description='Python package for instantaneous decision making in sports',
+    url='https://github.com/AbdullahKhurram30/Pythlete',
+    author='Muhammad Abdullah Khan',
+    author_email='abdullah.khurram@uni.minerva.edu',
+    packages=['pythlete'],
+    install_requires=['pandas']
+)
