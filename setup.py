a60d950f22e19d41572e510e5e4f1ca728f2b19d
Abdullah <abdullah.khurram@uni.minerva.edu>
2024-02-24 12:07:19 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Muhammad Abdullah Khan',
     author_email='abdullah.khurram@uni.minerva.edu',
     packages=['pythlete'],
-    install_requires=['fastf1==3.1.3', 'ipython', 'matplotlib==3.5.3',
-                      'numpy==1.22.4', 'pandas==1.4.4', 'scipy==1.7.3',
-                      'seaborn==0.11.1']
+    install_requires=['fastf1', 'ipython', 'matplotlib',
+                      'numpy', 'pandas', 'scipy',
+                      'seaborn']
 )
